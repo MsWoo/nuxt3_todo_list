d21adaf7f984dcdb12b478a51561b23f4de69d92
MsWoo <minsoo010@naver.com>
2024-03-03 19:42:41 +0900

    ToDoList 만들기 예제 #1-2

---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import {useToDoStore} from "~/stores/toDoStore";
+
+const toDo = useToDoStore()
+const newToDo = ref("")
+
+function addToDo() {
+  if (newToDo.value) {
+    toDo.addToDo(newToDo.value)
+    newToDo.value = ""
+  }
+}
+
+function sortRegistered() {
+  toDo.toDoList.sort((a, b) => a.id - b.id)
+}
+
+function sortLatest() {
+  toDo.toDoList.sort((a, b) => b.id - a.id)
+}
+
+</script>
+
+<template>
+  <section class="w-full m-auto">
+    <h1 class="mb-10 text-5xl font-bold text-center">ToDo List</h1>
+
+    <form class="mb-5", @submit.prevent="addToDo()">
+      <label for="toDo" class="block mb-3 text-xl font-bold">new ToDo</label>
+      <div class="flex justify-between gap-5">
+        <input
+            v-model="newToDo"
+            type="text"
+            id="toDo"
+            class="w-3/4 px-3 py-4 rounded text-black"
+            placeholder="할 일을 입력해 주세요."/>
+        <button class="w-1/4 bg-slate-500 rounded">추가</button>
+      </div>
+    </form>
+
+    <div v-if="toDo.toDoList.length > 0">
+      <div class="flex gap-2">
+        <button @click="sortRegistered()">등록순</button>
+<!--        <button>등록순</button>-->
+        <i>|</i>
+        <button @click="sortLatest()">최신순</button>
+<!--        <button>최신순</button>-->
+      </div>
+
+      <ul>
+        <li
+            v-for="(list, key) in toDo.toDoList"
+            :key="key"
+            class="flex justify-between items-center gap-5 mt-5 px-5 py-2 border rounded">
+          <p class="flex gap-5 text-lg">
+            <span>{{key+1}}.</span>{{list.content}}
+          </p>
+          <button class="px-5 py-2 bg-slate-500 rounded" @click="toDo.deleteToDo(list.id)">삭제</button>
+        </li>
+      </ul>
+
+      <button class="block w-1/3 mt-10 m-auto px-3 py-4 bg-green-400 rounded" @click="toDo.clearToDo()">전체 삭제</button>
+    </div>
+
+
+  </section>
+</template>
+
+<style scoped>
+</style>
