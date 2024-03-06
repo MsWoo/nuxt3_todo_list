789b8217f6596bf3c1e45c66b5df524b26dfc8d5
MsWoo <minsoo010@naver.com>
2024-03-06 14:00:04 +0900

    ToDoList 만들기 예제 #5 - 페이지 공통 meta 태그 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'TODO List',
+      meta: [
+        {name: 'description', content: '오늘의 할 일은 무엇인가요?'},
+        {property: 'og:title', content: '오늘의 할 일'},
+        {property: 'og:description', content: '오늘의 할 일은 무엇인가요?'}
+      ],
+      link: [
+        {rel: 'icon', type: 'image/x-icon', href: 'http://localhost:3000/favicon.ico'}
+      ]
+    }
+  },
   devtools: { enabled: true },
   modules: [
     '@pinia/nuxt'
